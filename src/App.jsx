8b88634510345ce0896a060d335b443e429fd254
SaboyaDev <saboyadev@gmail.com>
2023-02-12 16:53:38 -0500

    Added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { CssBaseline } from '@mui/material'
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<CssBaseline />
+			<h1>Welcome to CinemaNow!</h1>
+		</div>
+	)
 }
 
 export default App
